94bc533029f7ead93660cefb05651d216fbd886b
Erly-Winarni <erlywinarni157@gmail.com>
2025-06-13 23:46:51 +0800

    feat: menambah user session

--- ProFootballTrain/app/src/main/java/pftrain/models/UserSession.java
+++ ProFootballTrain/app/src/main/java/pftrain/models/UserSession.java
@@ -1,6 +1,20 @@
 package pftrain.models;
 
-public class UserSession {
+public final class UserSession {
+    private static UserSession instance;
+    private User currentUser;
 
-    
+    private UserSession() {}
+
+    public static synchronized UserSession getInstance() {
+        if (instance == null) {
+            instance = new UserSession();
+        }
+        return instance;
+    }
+
+    public void setCurrentUser(User user) { this.currentUser = user; }
+    public User getCurrentUser() { return this.currentUser; }
+    public String getCurrentUserRole() { return (currentUser != null) ? currentUser.getRole() : null; }
+    public void cleanSession() { this.currentUser = null; }
 }
